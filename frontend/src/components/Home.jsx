10e95e0f5a32198538cbd7aadb8bd1bf8fbfb392
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-01-19 11:03:11 +0800

    conditional loading while fetching data

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,17 +3,25 @@ import BlogList from './BlogList'
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
+        const fetchTimeout = setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(res => res.json())
+            .then(data => {
+                setBlogs(data)
+                setIsPending(false)
+            })
+        }, 1000)
+        return () => clearTimeout(fetchTimeout)
     }, [])
 
     return (
         <div className="home">
             <div className="blog-list">
                 <h2>All Blogs</h2>
+                {isPending && (<div>Loading...</div>)}
                 {blogs && <BlogList blogs={blogs}/>}
             </div>
         </div>
